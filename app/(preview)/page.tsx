a64a6510163e23119ecd4bcbdf6f2800cff0d7e1
Vuk Dukic <vuchina@icloud.com>
2026-04-01 13:19:48 -0700

    add

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -107,7 +107,6 @@ export default function Chat() {
   return (
     <div className="flex justify-center items-start sm:pt-16 min-h-screen w-full dark:bg-neutral-900 px-4 md:px-0 py-4">
       <div className="flex flex-col items-center w-full max-w-[500px]">
-        <ProjectOverview />
         <motion.div
           animate={{
             minHeight: isExpanded ? 200 : 0,
